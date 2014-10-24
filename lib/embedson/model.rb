6787974f59c5456ef1add2b88adc515f50ee0f11
sufleR <szymon.fracczak@netguru.co>
2014-10-24 19:44:27 +0200

    move method definitions to MethodBuilder class

--- lib/embedson/model.rb
+++ lib/embedson/model.rb
@@ -6,50 +6,7 @@ module Embedson
     end
 
     def embedded_in(name, options = {})
-      klass_name = (options.fetch(:class_name, nil) || name).to_s.classify
-      inverse_get = options.fetch(:inverse_of, nil) || self.name.demodulize.tableize.singularize
-      inverse_set = "#{inverse_get}="
-
-      define_method(name) do
-        instance_variable_get("@#{name}")
-      end
-
-      define_method("#{name}=") do |arg|
-        raise TypeError, "wrong argument type #{arg.class.name} (expected #{klass_name})" unless arg.nil? || arg.is_a?(klass_name.constantize)
-
-        instance_variable_set("@#{name}", arg)
-        parent = public_send(name)
-
-        if parent.respond_to?(inverse_set) && parent.public_send(inverse_get) != self
-          parent.public_send(inverse_set, self)
-        end
-      end
-
-      define_method('destroy') do
-        parent = public_send(name)
-        return false unless parent.present?
-        parent.public_send(inverse_set, nil)
-        parent.save!
-      end
-
-      define_method('save') do
-        parent = public_send(name)
-        return false unless parent.present?
-        parent.save
-      end
-
-      define_method('save!') do
-        parent = public_send(name)
-        raise "No parent model defined!" unless parent.present?
-        parent.save!
-      end
-
-      define_method('embedson_model_changed!') do
-        parent = public_send(name)
-        raise "No parent model defined!" unless parent.present?
-        parent.public_send(inverse_set, self)
-        true
-      end
+      MethodBuilder.new(self, name, options).embedded
     end
   end
 end
---
+++ lib/embedson/model/method_builder.rb
@@ -0,0 +1,132 @@
+module Embedson
+  module Model
+
+    class MethodBuilder
+
+      attr_reader :klass, :field_name, :options
+
+      def initialize(klass, name, options)
+        @klass = klass
+        @field_name = name
+        @options = options
+      end
+
+      def embeds
+        klass.class_exec field_name, related_klass_name, column_name, inverse_get, inverse_set, &embeds_writer
+        klass.class_exec field_name, related_klass_name, column_name, inverse_set, &embeds_reader
+      end
+
+      def embedded
+        klass.class_exec field_name, &embedded_reader
+        klass.class_exec field_name, related_klass_name, column_name, inverse_get, inverse_set, &embedded_writer
+        klass.class_exec field_name, &embedded_save
+        klass.class_exec field_name, inverse_set, &embedded_destroy
+        klass.class_exec field_name, inverse_set, &embedded_changed
+      end
+
+      private
+
+      def embeds_writer
+        lambda do |field_name, related_klass_name, column_name, inverse_get, inverse_set|
+          define_method("#{field_name}=") do |arg|
+            raise TypeError, "wrong argument type #{arg.class.name} (expected #{related_klass_name})" unless arg.nil? || arg.is_a?(related_klass_name.constantize)
+
+            if arg.respond_to?(inverse_set) && arg.public_send(inverse_get) != self
+              arg.public_send(inverse_set, self)
+            end
+
+            instance_variable_set("@#{field_name}", arg)
+            write_attribute(column_name, arg.nil? ? arg : arg.to_h)
+          end
+        end
+      end
+
+      def embeds_reader
+        lambda do |field_name, related_klass_name, column_name, inverse_set|
+          define_method(field_name) do
+            return if read_attribute(column_name).nil?
+
+            if instance_variable_get("@#{field_name}").nil?
+              model = related_klass_name.constantize.new(read_attribute(column_name))
+              instance_variable_set("@#{field_name}", model)
+              model.public_send(inverse_set, self) if model.respond_to?(inverse_set)
+            end
+            instance_variable_get("@#{field_name}")
+          end
+        end
+      end
+
+      def embedded_reader
+        lambda do |field_name|
+          define_method(field_name) do
+            instance_variable_get("@#{field_name}")
+          end
+        end
+      end
+
+      def embedded_writer
+        lambda do |field_name, related_klass_name, column_name, inverse_get, inverse_set|
+          define_method("#{field_name}=") do |arg|
+            raise TypeError, "wrong argument type #{arg.class.name} (expected #{related_klass_name})" unless arg.nil? || arg.is_a?(related_klass_name.constantize)
+
+            instance_variable_set("@#{field_name}", arg)
+            parent = public_send(field_name)
+
+            if parent.respond_to?(inverse_set) && parent.public_send(inverse_get) != self
+              parent.public_send(inverse_set, self)
+            end
+          end
+        end
+      end
+
+      def embedded_destroy
+        lambda do |field_name, inverse_set|
+          define_method('destroy') do
+            parent = public_send(field_name)
+            return false unless parent.present?
+            parent.public_send(inverse_set, nil)
+            parent.save!
+          end
+        end
+      end
+
+      def embedded_save
+        lambda do |field_name|
+          define_method('save') do
+            parent = public_send(field_name)
+            return false unless parent.present?
+            parent.save
+          end
+        end
+      end
+
+      def embedded_changed
+        lambda do |field_name, inverse_set|
+          define_method('embedson_model_changed!') do
+            parent = public_send(field_name)
+            raise "No parent model defined!" unless parent.present?
+            parent.public_send(inverse_set, self)
+            true
+          end
+        end
+      end
+
+      def column_name
+        @column_name ||= options.fetch(:column_name, nil) || field_name
+      end
+
+      def related_klass_name
+        @related_klass_name ||= (options.fetch(:class_name, nil) || field_name).to_s.classify
+      end
+
+      def inverse_get
+        @inverse_get ||= options.fetch(:inverse_of, nil) || klass.name.demodulize.tableize.singularize
+      end
+
+      def inverse_set
+        "#{inverse_get}="
+      end
+    end
+
+  end
+end
