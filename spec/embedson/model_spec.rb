c02f8abf11fc49489f5c54cd00aa79365892b69d
sufleR <szymon.fracczak@netguru.co>
2014-10-26 12:24:48 +0100

    tests for embedded_in

--- spec/embedson/model_spec.rb
+++ spec/embedson/model_spec.rb
@@ -245,4 +245,352 @@ describe Embedson::Model do
       end
     end
   end
+
+  describe '#embedded_in' do
+    context 'when only relation name is specified' do
+
+      with_model :Parent do
+        table do |t|
+          t.json :embedded
+        end
+
+        model do
+          extend Embedson::Model
+
+          embeds_one :embedded
+        end
+      end
+
+      class Embedded < OpenStruct
+        extend Embedson::Model
+
+        embedded_in :parent
+
+        def change=(arg)
+          @change = arg
+        end
+
+        def to_h
+          h = { defined: 'in', embedded: true }
+          h.merge!(change: @change) if @change.present?
+          h
+        end
+      end
+
+      let(:parent) { Parent.new() }
+      let(:embedded) { Embedded.new() }
+
+      it 'adds "embeds_one" method' do
+        expect(Embedded).to respond_to(:embedded_in)
+      end
+
+      it 'adds "parent" method' do
+        expect(embedded).to respond_to(:parent)
+      end
+
+      it 'adds "parent=" method' do
+        expect(embedded).to respond_to("parent=")
+      end
+
+      describe 'defined .parent= method' do
+        it 'assigns value of embeding class' do
+          expect {
+            embedded.parent = parent
+          }.to change{ embedded.parent }.from(nil).to(parent)
+        end
+
+        context 'when assigning nil' do
+          let(:embedded) { Embedded.new(parent: parent) }
+
+          it 'removes assignmnent' do
+            parent
+            expect {
+              embedded.parent = nil
+            }.to change { embedded.parent }.from(parent).to(nil)
+          end
+        end
+
+        context 'when assigning wrong class' do
+          it 'raises ClassTypeError' do
+            expect{
+              embedded.parent = 'something'
+            }.to raise_error(Embedson::ClassTypeError)
+          end
+        end
+
+        context 'when parent is persisted' do
+          before do
+            parent.save!
+          end
+
+          it 'marks parent as changed' do
+            expect {
+              embedded.parent = parent
+            }.to change{ parent.changed? }.from(false).to(true)
+          end
+
+          context 'and saved after change' do
+            it 'saves Embedded class in column' do
+              expect{
+                embedded.parent = parent
+                parent.save!
+              }.to change { parent.reload.read_attribute(:embedded) }.from(nil).to(embedded.to_h.stringify_keys)
+            end
+          end
+        end
+      end
+
+      describe 'defined .save method' do
+
+        context 'when there is parent' do
+          let(:embedded) { Embedded.new(parent: parent) }
+
+          it 'calls .save on parent' do
+            expect(parent).to receive(:save)
+            embedded.save
+          end
+
+          it 'saves to_h result in parent column' do
+            parent.save!
+            expect {
+              embedded.save
+            }.to change { parent.reload.read_attribute(:embedded) }.from(nil).to({ "defined" => "in", "embedded" => true })
+          end
+        end
+
+        context 'when there is no parent' do
+          let(:embedded) { Embedded.new }
+
+          it 'returns false' do
+            expect(embedded.save).to eq(false)
+          end
+        end
+      end
+
+      describe 'defined .save! method' do
+
+        context 'when there is parent' do
+          let(:embedded) { Embedded.new(parent: parent) }
+
+          it 'calls .save on parent' do
+            expect(parent).to receive(:save!)
+            embedded.save!
+          end
+
+          it 'saves to_h result in parent column' do
+            parent.save!
+            expect {
+              embedded.save!
+            }.to change { parent.reload.read_attribute(:embedded) }.from(nil).to({ "defined" => "in", "embedded" => true })
+          end
+        end
+
+        context 'when there is no parent' do
+          it 'raises argument error' do
+            expect { embedded.save! }.to raise_error(Embedson::NoParentError)
+          end
+        end
+      end
+
+      describe 'defined .destroy method' do
+
+        context 'when there is parent' do
+          let(:embedded) { Embedded.new(parent: parent) }
+
+          it 'calls .save! on parent' do
+            expect(parent).to receive(:save!)
+            embedded.destroy
+          end
+
+          it 'saves nil in parent column' do
+            embedded.save!
+            expect {
+              embedded.destroy
+            }.to change { parent.reload.read_attribute(:embedded) }.from({ "defined" => "in", "embedded" => true }).to(nil)
+          end
+        end
+
+        context 'when there is no parent' do
+          let(:embedded) { Embedded.new }
+
+          it 'returns false' do
+            expect(embedded.destroy).to eq(false)
+          end
+        end
+      end
+
+      describe 'defined .embedson_model_changed! method' do
+
+        context 'when there is parent' do
+          let(:embedded) { Embedded.new(parent: parent) }
+
+          it 'returns true' do
+            expect(embedded.embedson_model_changed!).to eq(true)
+          end
+
+          it 'assigns new value to parent' do
+            embedded.change = 'registered'
+            expect {
+              embedded.embedson_model_changed!
+            }.to change { parent.read_attribute(:embedded) }.from({ "defined" => "in", "embedded" => true }).to({ "defined" => "in", "embedded" => true, 'change' => 'registered' })
+          end
+        end
+
+        context 'when there is no parent' do
+          it 'raises argument error' do
+            expect { embedded.embedson_model_changed! }.to raise_error(Embedson::NoParentError)
+          end
+        end
+      end
+    end
+
+    context 'when options include inverse_of' do
+
+      with_model :Parent do
+        table do |t|
+          t.json :emb
+        end
+
+        model do
+          extend Embedson::Model
+
+          embeds_one :emb, class_name: Embedded
+        end
+      end
+
+      with_model :Embedded do
+        model do
+          extend Embedson::Model
+
+          embedded_in :parent, inverse_of: :emb
+
+          def to_h
+            { defined: 'in', embedded: true }
+          end
+        end
+      end
+
+      let(:parent) { Parent.new() }
+      let(:embedded) { Embedded.new() }
+
+      it 'adds "embeds_one" method' do
+        expect(Embedded).to respond_to(:embedded_in)
+      end
+
+      it 'adds "parent" method' do
+        expect(embedded).to respond_to(:parent)
+      end
+
+      it 'adds "parent=" method' do
+        expect(embedded).to respond_to("parent=")
+      end
+
+      it 'assigns self to inverse_of value in parent class' do
+        embedded.parent = parent
+        expect(embedded.parent.emb).to eq embedded
+      end
+    end
+
+    context 'when options include class_name' do
+
+      with_model :Parented do
+        table do |t|
+          t.json :embedded
+        end
+
+        model do
+          extend Embedson::Model
+
+          embeds_one :embedded
+        end
+      end
+
+      with_model :Embedded do
+        model do
+          extend Embedson::Model
+
+          embedded_in :parent, class_name: Parented
+
+          def to_h
+            { defined: 'in', embedded: true }
+          end
+        end
+      end
+
+      let(:parent) { Parented.new() }
+      let(:embedded) { Embedded.new() }
+
+      it 'adds "embeds_one" method' do
+        expect(Embedded).to respond_to(:embedded_in)
+      end
+
+      it 'adds "parent" method' do
+        expect(embedded).to respond_to(:parent)
+      end
+
+      it 'adds "parent=" method' do
+        expect(embedded).to respond_to("parent=")
+      end
+
+      describe 'defined .parent method' do
+        let(:embedded) { Embedded.new(parent: parent) }
+
+        it 'returns object of Parented class' do
+          expect(embedded.parent).to be_a(Parented)
+        end
+      end
+    end
+
+    context 'when parent model does not have defined embeds_one' do
+
+      with_model :Parent do
+        table do |t|
+          t.json :embedded
+        end
+
+        model do
+          extend Embedson::Model
+
+        end
+      end
+
+      with_model :Embedded do
+        model do
+          extend Embedson::Model
+
+          embedded_in :parent
+
+          def to_h
+            { defined: 'in', embedded: true }
+          end
+        end
+      end
+
+      let(:parent) { Parent.new() }
+      let(:embedded) { Embedded.new() }
+
+      it 'adds "embeds_one" method' do
+        expect(Embedded).to respond_to(:embedded_in)
+      end
+
+      it 'adds "parent" method' do
+        expect(embedded).to respond_to(:parent)
+      end
+
+      it 'adds "parent=" method' do
+        expect(embedded).to respond_to("parent=")
+      end
+
+      describe 'defined .save method' do
+        let(:embedded) { Embedded.new(parent: parent) }
+
+        it 'saves parent but not changes embedded column' do
+          parent.save!
+          expect {
+            embedded.save!
+          }.not_to change { parent.reload.embedded }.from(nil)
+        end
+      end
+    end
+  end
 end
